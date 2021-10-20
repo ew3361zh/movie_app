52314b23c005923bfbbf18ce548b535d7ae16fc0
Sonic <sf7293gv@go.minnstate.edu>
2021-10-20 18:26:42 -0500

    Modified tmdb.py code to functions to access to it from main.py

--- apis/tmdb.py
+++ apis/tmdb.py
@@ -12,15 +12,17 @@ tmdb_key = os.environ.get('TMDB_KEY')
 
 tmdb_url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=0f6e4ae275505f59ad1f7ea5b93d5f6f&language=en-US&page=1&region=US'
 
+""" This function will return a list movies from the api """
+def get_movie_titles():
+    try:
+        movie_data = requests.get(tmdb_url).json()
+        results = movie_data['results']
+        movie_titles = []
+        for movie in results:
+            title = movie['title']
+            movie_titles.append(title)
 
-def get_movie_data():
-
-    movie_data = requests.get(tmdb_url).json()
-    results = movie_data['results']
-    movie_titles = []
-    for movie in results:
-        title = movie['title']
-        movie_titles.append(title)
-
-    pprint(movie_titles)
-    return(movie_titles)
+        pprint(movie_titles)
+        return(movie_titles)
+    except:
+        print()
