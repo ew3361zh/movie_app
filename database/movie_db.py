513c9cde420db47d61a7d67649d4e24dd1dcdb08
ndyt <ntomlinson@gmail.com>
2021-10-20 16:39:43 -0500

    create table function written in movie_db"

--- database/movie_db.py
+++ database/movie_db.py
@@ -1,3 +1,26 @@
 import sqlite3
 from .config import db_path
 from model.movie_model import Movie
+from exceptions.movie_error import MovieError
+
+db = db_path
+
+class MovieDB():
+
+    #def create_table
+    def __init__(self):
+        with sqlite3.connect(db) as conn:
+            conn.execute("""CREATE TABLE IF NOT EXISTS movies (
+                        title TEXT NOT_NULL,
+                        director TEXT,
+                        release_date TEXT,
+                        actor_1 TEXT,
+                        actor_2 TEXT,
+                        poster_img TEXT,
+                        genre TEXT,
+                        rating TEXT,
+                        plot_summary TEXT,
+                        youtube_link TEXT)"""
+            )
+        conn.close()
+    
