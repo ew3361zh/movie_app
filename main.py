64ae41f10ef75b2f1f688267c25c1a82cdc3592f
Sonic <sf7293gv@go.minnstate.edu>
2021-10-20 21:45:32 -0500

    Workin on the movie.html file to show results to user

--- main.py
+++ main.py
@@ -11,8 +11,15 @@ def home_page():
 @app.route('/get-movie')
 def get_movie():
     movie_titles = tmdb.get_movie_titles()
+    #title = user choice
     movie_data = omdb.get_movie_data()
     movie_trailer_id = youtube_api.movie_trailer()
+    movie_title = ""
+    movie_poster = ""
+    video_id = ""
+    movie_info = ""
+    return render_template('movie.html', title=movie_title, data=movie_info,poster=movie_poster, videoID=video_id)
+
 
 if __name__ == '__main__':
     app.run()
