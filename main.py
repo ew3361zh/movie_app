f55fa68fd256cf903e734fb6ec7a7a10f51ef737
Sonic <sf7293gv@go.minnstate.edu>
2021-10-24 21:33:30 -0500

    Modified main.py and index.html to fetch movie from api and display them.

--- main.py
+++ main.py
@@ -6,19 +6,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('index.html')
-
-@app.route('/get-movie')
-def get_movie():
+    print('hi')
     movie_titles = tmdb.get_movie_titles()
-    #title = user choice
-    movie_data = omdb.get_movie_data()
-    movie_trailer_id = youtube_api.movie_trailer()
-    movie_title = ""
-    movie_poster = ""
-    video_id = ""
-    movie_info = ""
-    return render_template('movie.html', title=movie_title, data=movie_info,poster=movie_poster, videoID=video_id)
+    
+    print(movie_titles)
+    return render_template('index.html', movieTitles = movie_titles)
+
+# @app.route('/get-movie')
+# def get_movie():
+    
+#     #title = user choice
+#     movie_data = omdb.get_movie_data()
+#     movie_trailer_id = youtube_api.movie_trailer()
+#     movie_title = ""
+#     movie_poster = ""
+#     video_id = ""
+#     movie_info = ""
+#     return render_template('movie.html', title=movie_title, data=movie_info,poster=movie_poster, videoID=video_id)
 
 
 if __name__ == '__main__':
