ef32f1135aa614e81a1db50bcdc80378a3bcdb85
Sonic <sf7293gv@go.minnstate.edu>
2021-10-24 23:25:07 -0500

    Modified main to work with create_new_movie.py to get  movie from api or db

--- apis/omdb.py
+++ apis/omdb.py
@@ -14,10 +14,10 @@ omdb_key = os.environ.get('OMDB_KEY')
 
 
 
-def get_movie_data():  
+def get_movie_data(title):  
     try: 
         # movie_title = input('Please enter a movie title you would like more info on: ')
-        omdb_url = f'http://www.omdbapi.com/?apikey={omdb_key}&t={movie_title}'
+        omdb_url = f'http://www.omdbapi.com/?apikey={omdb_key}&t={title}'
         movie_data = requests.get(omdb_url).json()
         pprint(movie_data)
         return movie_data
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, render_template, redirect
 from apis import omdb, tmdb, youtube_api
-#from database import db 
+from database.movie_db import MovieDB
+from create_new_movie import create_new_movie
 
 app = Flask(__name__)
+movie_db = MovieDB() # create the DB
 
 @app.route('/')
 def home_page():
     print('hi')
     movie_titles = tmdb.get_movie_titles()
     
-    print(movie_titles)
+    #print(movie_titles)
     return render_template('index.html', movieTitles = movie_titles)
 
 @app.route('/get-movie')
@@ -17,14 +19,19 @@ def get_movie():
     
     title = request.args.get('title')
     print(title)
-    movie_data = omdb.get_movie_data()
-    movie_trailer_id = youtube_api.movie_trailer()
-    movie_title = ""
-    movie_poster = ""
-    video_id = ""
-    movie_info = ""
-    return render_template('movie.html', title=movie_title, data=movie_info,poster=movie_poster, videoID=video_id)
+    # check if movie in SQL db
+    movie_check = movie_db.check_movie_in_db(title)
+    if movie_check == None: # None only returned from line 22 if movie not in DB
+        movie_data = omdb.get_movie_data(title)
+        movie_trailer_id = youtube_api.movie_trailer(title)
+        new_movie = create_new_movie(movie_data, movie_trailer_id)
+        movie_db.add_movie_to_db(new_movie)
+        movie_info = ""
+    else:
+        movie_check.title
+        # we'll have a movie object from the sql db
 
+    return render_template('movie.html', title=title, data=movie_info, poster=new_movie.poster_img, videoID=new_movie.youtube_id)
 
 if __name__ == '__main__':
     app.run()
