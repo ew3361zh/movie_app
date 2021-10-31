bcea16e02b2ce278912061c385049e89a9375a53
Sonic <sf7293gv@go.minnstate.edu>
2021-10-30 22:30:19 -0500

    Started to transfer Niko's files/folder in to my branch due an error. Copied the favorites_db.py file

---
+++ database/favorites_db.py
@@ -0,0 +1,94 @@
+import sqlite3
+from config import db_path
+from exceptions.movie_error import MovieError
+# from models.movie_model import Favorite # import later
+
+db = db_path
+
+class FavoritesDB():
+    
+    def __init__(self):
+        #create table function
+        with sqlite3.connect(db) as conn:
+            conn.execute("""CREATE TABLE IF NOT EXISTS favorites (
+                        tmdb_id TEXT NOT NULL UNIQUE, 
+                        title TEXT NOT_NULL,
+                        director TEXT,
+                        release_date TEXT,
+                        actor_1 TEXT,
+                        actor_2 TEXT,
+                        poster_img TEXT,
+                        genre TEXT,
+                        rating TEXT,
+                        plot_summary TEXT,
+                        youtube_id TEXT)"""
+                        )
+
+        # conn.close()    
+
+
+    # fetch all favorite movies from db
+    def get_all_favorites(self):
+
+        with sqlite3.connect(db) as conn:
+            try:
+                conn.row_factory = sqlite3.Row
+                results_query = conn.execute(f'SELECT * FROM favorites')
+                all_favorites = results_query.fetchall()
+                return all_favorites
+            except:
+                raise movie_error.MovieError('Problem fetching all favorites')
+
+    # add movie to favorites db
+    def add_favorite(self, movie):
+        # TODO make sure movie object sent here is getting tmdb_id
+        # TODO write test to make sure it's not adding a movie that's already in the DB
+        check_movie = self.check_movie_in_db(movie.tmdb_id)
+        if check_movie == None: 
+            with sqlite3.connect(db) as conn:
+                try:
+                    conn.execute(f'INSERT INTO favorites VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                                (movie.tmdb_id,
+                                movie.title,
+                                movie.director,
+                                movie.release_date,
+                                movie.actor_1,
+                                movie.actor_2,
+                                movie.poster_img,
+                                movie.genre,
+                                movie.rating,
+                                movie.plot_summary,
+                                movie.youtube_id))
+                    return True
+                except:
+                    raise MovieError('Problem adding favorite to db')
+        else:
+            return False
+    
+    #TODO delete favorite from DB function
+    
+    def check_movie_in_db(self, tmdb_id):
+        # check if movie title selected is already in db, return whole movie object if it is
+        with sqlite3.connect(db) as conn:
+            try:
+                conn.row_factory = sqlite3.Row
+                results_query = conn.execute('SELECT * FROM favorites WHERE tmdb_id = ?', (tmdb_id,))
+                results = results_query.fetchone()
+                if results == None:
+                    return None # or return False/True
+                else:
+                    requested_movie = Favorite(results[0],
+                                            results[1],
+                                            results[2],
+                                            results[3],
+                                            results[4],
+                                            results[5],
+                                            results[6],
+                                            results[7],
+                                            results[8],
+                                            results[9],
+                                            results[10]
+                                            )
+                    return requested_movie
+            except:
+                raise MovieError('Problem fetching movie from db')
