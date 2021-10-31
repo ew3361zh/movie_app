c5505f0b50c1b49d33e64f23954d85bf778d4f51
abdiawali <abdiongithub2019>
2021-10-30 22:31:49 -0500

    added error message

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ movie_db = MovieDB() # create the DB
 @app.route('/')
 def home_page():
     # get data from api, or cache and return data or none/error
-    movie_dict, error= tmdb.get_movie_titles()
+    movie_dict= tmdb.get_movie_titles()
 
     if movie_dict:
         movie_list=list(movie_dict['title'])
+        return render_template('index.html', movieTitles = movie_list)
     else:
-        # an error html?????????????????????????????
-        movie_list='an error has occured'
-    return render_template('index.html', movieTitles = movie_list)
+        error_message='an error has occured/ unable to fetch movies'
+        return render_template('error.html', error_message = error_message)
 
 @app.route('/get-movie')
 def get_movie():
